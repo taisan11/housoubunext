e286a77f3f675d5d878806452666f5f5b3e8c4a4
BuildTools <unconfigured@null.spigotmc.org>
2023-07-07 14:21:47 +0900

    a

--- next.config.js
+++ next.config.js
@@ -3,4 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+// バンドルアナライザ
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+const plugins = [
+  withBundleAnalyzer
+];
+
+
+module.exports = withPlugins(plugins, nextConfig)
