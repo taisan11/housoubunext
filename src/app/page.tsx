3f78700ea8f709790ccc74ef0f6e3aaea46e64ff
BuildTools <unconfigured@null.spigotmc.org>
2023-07-07 18:34:51 +0900

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,12 @@ import styles from './page.module.css'
 
 export default function Home() {
   return (
+    <>
     <p>ども</p>
+    <p>みなさんこんにちは</p>
+    <p>いまだにnext.jsの使い方がわからない人です</p>
+    <p>何この空要素</p>
+    <p>何で必要なんだ!!</p>
+    </>
   )
 }
