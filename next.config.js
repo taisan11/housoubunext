2d196c2a1ff8a97984e2720ddc8a439d68d02011
BuildTools <unconfigured@null.spigotmc.org>
2023-07-20 14:14:27 +0900

    1

--- next.config.js
+++ next.config.js
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
-const withPlugins = require('next-compose-plugins');
-// バンドルアナライザ
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
+const nextConfig = {}
 
-const plugins = [
-  withBundleAnalyzer
-];
-
-
-module.exports = withPlugins(plugins, nextConfig)
+module.exports = nextConfig
--- src/app/test/[nuko]/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page({ params }: { params: { nuko: string } }) {
-    return <div>My Post: {params.nuko}</div>
-  }
